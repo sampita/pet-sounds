fa10b3bce882933eb4c33c8c3b4b6f1faad12aa4
Sam Pita <samanthakpita@gmail.com>
2019-10-11 15:52:09 -0500

    represent your pet exercise

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+console.log('you is strong. you is smart. you is blessed.')
+
+const myPet = {
+    name: "Rosie",
+    species: "dog",
+    nicknames: [
+        "Roslyn J DizzleButt",
+        "Rosita Bonita",
+        "Ol Graybeard"
+    ],
+    age: "2 years"
+}
+
+console.log(myPet);
